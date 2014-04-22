92e153b8958b213786ec506c438e72cd5e681383
JJProgram <juanjopalopoli@hotmail.com>
2014-04-22 20:50:08 -0300

    TP 1 Ejercicios 1 - 13
    Los ejercicios del 1 al 13, faltaria verificar el sen(x) y e^x ya que
    son los que dan problemas con ciertos valores (principalmente e^x)

---
+++ TP1/TP1Ej1-13/FcMatematicas.h
@@ -0,0 +1,20 @@
+#ifndef FCMATEMATICAS_H_INCLUDED
+#define FCMATEMATICAS_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#define absol(x) (x)<0?-(x):(x)
+int selecsimp ();
+int factorial(int );
+int combinatoria (int , int );
+float potenciar (float ,int );
+float exponencial (int , float );
+float raizcuadrada(float ,float );
+float seno(float , float );
+void clasifnatural (int );
+int prodsumsuces(int ,int );
+int cocentero(int ,int ,int* ,int* );
+int sumprimn(int );
+int sumprimnpar (int );
+int esprimo (int );
+
+#endif // FCMATEMATICAS_H_INCLUDED
