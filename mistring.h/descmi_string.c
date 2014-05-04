cdfd6b1318a3a134780f3f982323db1da981bb40
JJProgram <juanjopalopoli@hotmail.com>
2014-05-04 14:35:50 -0300

    Mi version de string.h
    Funciones:
    str_chr
    str_cmp
    str_cmpi
    str_ncmp
    str_str
    str_cat
    str_cpy
    str_len
    str_upr
    str_lwr
    NOTA: Falta verificar strcmpi

--- mistring.h/descmi_string.c
+++ mistring.h/descmi_string.c
@@ -3,18 +3,21 @@
 #define aMinusc(X) (X)>='A'&&(X)<='Z'?(X)+32:(X)
 
 char * str_chr(const char* cbus,int c)
-{char * caux=cbus;
- while(*cbus&&*cbus!=c)
-   cbus++;
- return *cbus==c?cbus:NULL;
+{
+    char * caux=cbus;
+    while(*cbus&&*cbus!=c)
+        cbus++;
+    return *cbus==c?cbus:NULL;
 }
 
 int str_cmp (const char* cad1,const char* cad2)
-{while(*cad1&&*cad2&&!(*cad1-*cad2))
-   {cad1++;
-    cad2++;
-   }
-return *cad1-*cad2;
+{
+    while(*cad1&&*cad2&&!(*cad1-*cad2))
+    {
+        cad1++;
+        cad2++;
+    }
+    return *cad1-*cad2;
 }
 int str_cmpi(const char* cad1,const char* cad2)
 {
@@ -22,7 +25,8 @@ int str_cmpi(const char* cad1,const char* cad2)
     a=aMinusc(*cad1);
     b=aMinusc(*cad2);
     while(*cad1&&*cad2&&(a-b)==0)
-    {   cad1++;
+    {
+        cad1++;
         cad2++;
         a=aMinusc(*cad1);
         b=aMinusc(*cad2);
@@ -31,82 +35,97 @@ int str_cmpi(const char* cad1,const char* cad2)
 }
 
 int str_ncmp(const char* c1, const char* c2,int cmax)
-{while(cmax>0&&*c1&&*c2&&!(*c1-*c2))
-    {c1++;
-     c2++;
-     cmax--;
+{
+    while(cmax>0&&*c1&&*c2&&!(*c1-*c2))
+    {
+        c1++;
+        c2++;
+        cmax--;
     }
- return *c1-*c2;
+    return *c1-*c2;
 }
 
 char * str_str (const char* cad,const char* subcad)
-{char *caux=NULL,*subaux=subcad;
- while(*cad&&*subcad&&!caux)
+{
+    char *caux=NULL,*subaux=subcad;
+    while(*cad&&*subcad&&!caux)
     {
-     while(*cad!=*subcad&&*cad)
-        cad++;
-     caux=cad;
-     while(*cad&&*subcad&&*cad==*subcad)
-        {cad++;
-         subcad++;
+        while(*cad!=*subcad&&*cad)
+            cad++;
+        caux=cad;
+        while(*cad&&*subcad&&*cad==*subcad)
+        {
+            cad++;
+            subcad++;
         }
-     if(*subcad)
-       {caux=NULL;
-        subcad=subaux;
-       }
+        if(*subcad)
+        {
+            caux=NULL;
+            subcad=subaux;
+        }
     }
- return caux;
+    return caux;
 }
 
 char * str_cat (char* cad1,const char* cad2)
-{char* aux=cad1;
- if(!*cad2)
-    return cad1;
- while(*cad1)
-    cad1++;
- while(*cad2)
-    {*cad1=*cad2;
-     cad1++;
-     cad2++;
+{
+    char* aux=cad1;
+    if(!*cad2)
+        return cad1;
+    while(*cad1)
+        cad1++;
+    while(*cad2)
+    {
+        *cad1=*cad2;
+        cad1++;
+        cad2++;
     }
- *cad1='\0';
- return aux;
+    *cad1='\0';
+    return aux;
 }
 
 char * str_cpy (char* cad1,const char* cad2)
-{char* aux=cad1;
- while(*cad2)
-    {*cad1=*cad2;
-     cad1++;
-     cad2++;
+{
+    char* aux=cad1;
+    while(*cad2)
+    {
+        *cad1=*cad2;
+        cad1++;
+        cad2++;
     }
- *cad1='\0';
- return aux;
+    *cad1='\0';
+    return aux;
 }
 
 int str_len (const char* cad)
-{int c=0;
- while(*cad)
-    {c++;
-     cad++;
+{
+    int c=0;
+    while(*cad)
+    {
+        c++;
+        cad++;
     }
- return c;
+    return c;
 }
 
 char * str_upr (char* cad)
-{char* caux=cad;
- while(*cad)
-   {*cad=aMayusc(*cad);
-    cad++;
-   }
- return caux;
+{
+    char* caux=cad;
+    while(*cad)
+    {
+        *cad=aMayusc(*cad);
+        cad++;
+    }
+    return caux;
 }
 
 char * str_lwr (char* cad)
-{char* caux=cad;
- while(*cad)
-   {*cad=aMinusc(*cad);
-    cad++;
-   }
- return caux;
+{
+    char* caux=cad;
+    while(*cad)
+    {
+        *cad=aMinusc(*cad);
+        cad++;
+    }
+    return caux;
 }
