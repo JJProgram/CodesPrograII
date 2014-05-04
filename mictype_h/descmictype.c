d61512204beaca129477701ccca64d4890ded808
JJProgram <juanjopalopoli@hotmail.com>
2014-05-04 14:33:34 -0300

    Mi version de ctype.h
    Funciones presentes:
    is_alpha
    is_digit
    to_upper
    to_lower

--- mictype_h/descmictype.c
+++ mictype_h/descmictype.c
@@ -1,17 +1,21 @@
 #include "mictype.h"
 
 int is_alpha (int c)
-{return ((c>='a'&&c<='z')||(c>='A'&&c<='Z'));
+{
+    return ((c>='a'&&c<='z')||(c>='A'&&c<='Z'));
 }
 
 int is_digit (int c)
-{return (c>='0'&&c<='9');
+{
+    return (c>='0'&&c<='9');
 }
 
 int to_upper (int c)
-{return ((c>='a'&&c<='z')?c-32:c);
+{
+    return ((c>='a'&&c<='z')?c-32:c);
 }
 
 int to_lower (int c)
-{return ((c>='A'&&c<='Z')?c+32:c);
+{
+    return ((c>='A'&&c<='Z')?c+32:c);
 }
