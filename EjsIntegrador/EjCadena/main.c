3a15bb6bb887ace47a3b8e7f0aa8f1a1ef2cb3e6
JJProgram <juanjopalopoli@hotmail.com>
2014-06-26 22:14:40 -0300

    Ejercicios Integradores
    De momento, el ejercicio de cadena y el de matriz

---
+++ EjsIntegrador/EjCadena/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char cadena1[40]={"Copa lata"},cadena2[40]={"Hola lorena"};
+    printf("\"%s\" y \"%s\"\n",cadena1,cadena2);
+    printf("Se encontraron %d ocurrencias\n",eliminarOcurrencias(cadena1,cadena2));
+    printf("\"%s\" y \"%s\"\n",cadena1,cadena2);
+    return 0;
+}
+int eliminarOcurrencias(char* cad1,char* cad2)
+{
+    int contCoinc;
+    char* pl1,*pe1,*pl2,*pe2;
+    if(!*cad1||!*cad2)
+        return 0; //si alguna de las cadenas es nula, directamente se que no habra coincidencias
+    contCoinc=0;
+    pl1=pe1=cad1;
+    pl2=pe2=cad2;
+    while(*pl1&&*pl2)
+    {
+        if(*pl1!=*pl2)
+        {
+            *pe1=*pl1;
+            *pe2=*pl2;
+            pl1++;
+            pl2++;
+            pe1++;
+            pe2++;
+        }
+        else
+        {
+            contCoinc++;
+            pl1++;
+            pl2++;
+        }
+    }
+    while(*pl1&&!*pl2)
+    {
+        *pe1=*pl1;
+        pl1++;
+        pe1++;
+    }
+    while(!*pl1&&*pl2)
+    {
+        *pe2=*pl2;
+        pl2++;
+        pe2++;
+    }
+    *pe1='\0';
+    *pe2='\0';
+    return contCoinc;
+}
---
+++ EjsIntegrador/EjMatriz/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FIL 10
+#define COL 20
+
+int main()
+{
+    /*Este main esta hecho para probar, si se quiere hacer una funcion de insercion
+    hay que tener en cuenta que tiene que tener 2 punteros a int para el numero de filas
+    y columnas, y que el valor que conserven siga lo enunciado en la logica de traspuesta*/
+    int mat[FIL][COL]={{1,2,3},{4,5,6},{7,8,9}};
+    mostrarMat(mat,3,3);
+    traspuestaInSituCuadrada(mat,3,3);
+    mostrarMat(mat,3,3);
+}
+int traspuestaInSituCuadrada (int m[][COL],int nfilmat,int ncolmat)
+{
+    /*nfilmat: nº de filas ocupados en la matriz
+      ncolmat: nº de columnas ocupados en la matriz
+      Si tengo 1 fila, subindice 0, 2 filas, 0 y 1, etc. Lo mismo para columnas*/
+    int aux,i,j;
+    if(nfilmat!=ncolmat)
+        return 0; //no es cuadrada.
+    for(i=0;i<nfilmat;i++)
+        for(j=i;j<ncolmat;j++)
+        {
+            aux=m[i][j];
+            m[i][j]=m[j][i];
+            m[j][i]=aux;
+        }
+    return 1;
+}
+void mostrarMat(int m[][COL],int nfilmat,int ncolmat)
+{
+    int i,j;
+    for(i=0;i<nfilmat;i++)
+    {
+        for(j=0;j<ncolmat;j++)
+            printf("%d ",m[i][j]);
+        printf("\n");
+    }
+}
